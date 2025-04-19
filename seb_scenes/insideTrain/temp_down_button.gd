5b85c07dc2c5dd939a76b83ddaae9b96dd84075e
Sam Skinner <sebs@Sams-MacBook-Pro-3.local>
2025-04-19 10:43:19 -0400

    added snow shader

--- seb_scenes/insideTrain/temp_down_button.gd
+++ seb_scenes/insideTrain/temp_down_button.gd
@@ -1,12 +1,4 @@
 extends Interactible3D
 
-#var time_alive: float
-
-#func _process(delta: float) -> void:
-	#pass
-	#time_alive += delta;
-	#get_parent().get_node("SubViewport/InsideTrainUi").temperature_gague_normalized = fmod(time_alive, 1.0);
-	#get_parent().get_node("SubViewport/InsideTrainUi").pressure_gague_normalized = fmod(time_alive, 1.0);
-
 func _interact():
 	get_parent().get_node("SubViewport/InsideTrainUi").temperature_gague_normalized -= randf_range(0.001, 0.01);
