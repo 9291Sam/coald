7465bf36b275dbc8b46a41896f938ced1d879f77
Sam Skinner <sebs@Sams-MacBook-Pro-3.local>
2025-04-19 14:37:46 -0400

    sync

--- seb_scenes/insideTrain/InsideTrain.gd
+++ seb_scenes/insideTrain/InsideTrain.gd
@@ -1,6 +1,15 @@
 extends Node3D
 
-var time_alive = 0.0;
+var time_traveling = 0.0;
+const THIS_TRAVEL_TIME = 5.0;
+
+enum InsideTrainState {
+	BeforeMoving,
+	Moving,
+	AfterMoving
+}
+
+var state: InsideTrainState = InsideTrainState.BeforeMoving;
 
 
 func fract(x: float) -> float:
@@ -13,7 +22,6 @@ func my_hash(x1: float) -> float:
 func interp(a: float, b: float, t: float) -> float:
 	var smooth_t = t * t * (3.0 - 2.0 * t) # smoothstep
 	return a * (1.0 - smooth_t) + b * smooth_t
-
 
 # Clean 1D Perlin-like smooth noise function for Godot
 func smooth_noise_1d(t: float, frequency: float = 4.0) -> float:
@@ -34,36 +42,42 @@ func _ready():
 	
 
 func _process(delta: float) -> void:
-	time_alive += delta;
+	match state:
+		InsideTrainState.BeforeMoving:	
+			$Lever.lever_state_inorm = 0.5;
+			
+			if $Lever/lever_pull.is_active:
+				$Lever/lever_pull.CanInteract = false;
+				$Lever.lever_state_inorm = 0.0;
+				state = InsideTrainState.Moving;
+			pass;
+		InsideTrainState.Moving:
+			time_traveling += delta;
+			read_gague_manipulators();
+			
+			if time_traveling > THIS_TRAVEL_TIME:
+				$SubViewport/InsideTrainUi.enable_light = true;
+				$Lever/lever_pull.CanInteract = true;
+				if $Lever/lever_pull.is_active:
+					$Lever.lever_state_inorm = 0.5;
+					state = InsideTrainState.AfterMoving;
+					
+					
+			pass;
+		InsideTrainState.AfterMoving:
+			$leave_train_button.CanInteract = true;
+			if $leave_train_button.is_active:
+				get_tree().change_scene_to_file("res://seb_scenes/lever/lever_pull.tscn")
+			pass;
+			
+	print("Current State: %d | time_till_over %f" % [state, THIS_TRAVEL_TIME - time_traveling])
+
+			
 	
-	var p = smooth_noise_1d(time_alive + 93942.3, 0.03)
-	var p2 = smooth_noise_1d(time_alive - 83849.3, 0.03)
+	move_gagues(delta);
+		
 	
-	var random_smooth_add_normalized = smooth_noise_1d(time_alive, p / 6); # 16
-	var random_smooth_add_normalized2 = smooth_noise_1d(time_alive, p2 / 6); # 16
-	
-	const pressure_normal_base = 0.1 - 0.06;
-	const pressure_normal_peak = 0.65 + 0.23;
-	
-	const temperature_normal_base = 0.17 - 0.12;
-	const temperature_normal_peak = 0.54 + 0.32;
-	
-	var green_zone_rand_temoerature = remap(random_smooth_add_normalized, 0.0, 1.0, temperature_normal_base, temperature_normal_peak);
-	var green_zone_rand_pressure = remap(random_smooth_add_normalized2, 0.0, 1.0, pressure_normal_base, pressure_normal_peak);
-	$SubViewport/InsideTrainUi.pressure_gague_normalized = green_zone_rand_temoerature + pressure_velocity;
-	$SubViewport/InsideTrainUi.temperature_gague_normalized = green_zone_rand_pressure + temp_velocity;
-	
-	pressure_velocity += randf_range(-0.03, 0.03) * delta;
-	temp_velocity += randf_range(-0.03, 0.03) * delta;
-	
-	print("Pressure: %f | Temp: %f" % [pressure_velocity, temp_velocity]);
-	
-	temp_velocity *= pow(normalization_factor, delta);
-	pressure_velocity *= pow(normalization_factor, delta);
-	
-	#temp_velocity *= (temp_velocity * normalization_factor * delta);
-	#pressure_velocity *= (pressure_velocity * normalization_factor * delta);
-	
+func read_gague_manipulators():
 	if $temp_up_button.is_active:
 		temp_velocity += randf_range(0.01, 0.04);
 		
@@ -76,14 +90,28 @@ func _process(delta: float) -> void:
 	if $pressure_down_button.is_active:
 		pressure_velocity -= randf_range(0.01, 0.04);
 	
-	if $leave_train_button.is_active:
-		get_tree().change_scene_to_file("res://seb_scenes/lever/lever_pull.tscn")
-		
-	if $Lever/lever_pull.is_active:
-		if $Lever.lever_state_inorm == 0.0:
-			$Lever/lever_pull.CanInteract = false;
-			$leave_train_button.CanInteract = true;
-			$Lever.lever_state_inorm = 0.5;
-		
+func move_gagues(delta):
+	var p = smooth_noise_1d(time_traveling + 93942.3, 0.03)
+	var p2 = smooth_noise_1d(time_traveling - 83849.3, 0.03)
+	
+	var random_smooth_add_normalized = smooth_noise_1d(time_traveling, p / 6); # 16
+	var random_smooth_add_normalized2 = smooth_noise_1d(time_traveling, p2 / 6); # 16
+	
+	const pressure_normal_base = 0.1 - 0.06;
+	const pressure_normal_peak = 0.65 + 0.23;
+	
+	const temperature_normal_base = 0.17 - 0.12;
+	const temperature_normal_peak = 0.54 + 0.32;
+	
+	pressure_velocity += randf_range(-0.03, 0.03) * delta;
+	temp_velocity += randf_range(-0.03, 0.03) * delta;
+	
+	temp_velocity *= pow(normalization_factor, delta);
+	pressure_velocity *= pow(normalization_factor, delta);
+	
+	var green_zone_rand_temoerature = remap(random_smooth_add_normalized, 0.0, 1.0, temperature_normal_base, temperature_normal_peak);
+	var green_zone_rand_pressure = remap(random_smooth_add_normalized2, 0.0, 1.0, pressure_normal_base, pressure_normal_peak);
+	$SubViewport/InsideTrainUi.pressure_gague_normalized = green_zone_rand_temoerature + pressure_velocity;
+	$SubViewport/InsideTrainUi.temperature_gague_normalized = green_zone_rand_pressure + temp_velocity;
 	
 	
